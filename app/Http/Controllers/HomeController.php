dd56100c2a89a9fad870439fdb072ee378a5ecb1
Faryaz <36654269+Faryaz-Ch@users.noreply.github.com>
2018-05-12 00:15:58 +1200

    Update HomeController.php
    Protection on Home page removed.
    Now homepage will not redirect to login page for auth.
    Home Page Redirection Fixed.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,16 +11,23 @@ class HomeController extends Controller
      *
      * @return void
      */
+     
+    //Removing authorisation for home page below.
+    
+    /* 
     public function __construct()
     {
         $this->middleware('auth');
     }
+    */
 
     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
      */
+
+    //This will show home page (Modification by Faryaz)
     public function index()
     {
         return view('welcome');
